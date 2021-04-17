6382d680d932f9d40a50298f74b4628a4b584e21
Dinh Huu <huutc123@gmail.com>
2021-04-17 22:00:41 +0700

    Hoan thien font-end v1

--- demo/fe/public/js/dialog.js
+++ demo/fe/public/js/dialog.js
@@ -11,25 +11,7 @@
 
 function dialog(dialogType){
     switch(dialogType){
-        case 'uploadDialog':{
-            return `
-            <div class="dialog" id="upload-dialog">
-                <div class="heading">
-                    <p>Tải bài hát lên</p>
-                    <i class="fas fa-times cancel"></i>
-                </div>
-                <div class="content">
-                    <input type="file" name="upload" id="upload" accept=".mp3" multiple>
-                </div>
-                <div class="footer">
-                    <button class="btn btn-info">Xác nhận</button>
-                    <button class="btn btn-seco cancel">Hủy bỏ</button>
-                </div>
-            </div>
-            
-            `;
-        }
-        case 'deleteDialog':{
+        case 'deleteMusicDialog':{
             return `
             <div class="dialog" id="delete-dialog">
                 <div class="heading">
@@ -44,27 +26,20 @@ function dialog(dialogType){
                     <button class="btn btn-seco cancel">Hủy bỏ</button>
                 </div>
             </div>
-            
             `;
         }
-        case 'selectPlaylistDialog':{
+        case 'deletePlayListDialog':{
             return `
-            <div class="dialog" id="select-playlist-dialog">
+            <div class="dialog" id="delete-dialog">
                 <div class="heading">
-                    <p>Thêm vào PlayList</p>
+                    <p>Xóa PlayList</p>
                     <i class="fas fa-times cancel"></i>
                 </div>
                 <div class="content">
-                    <label for="select-playlist">Chọn PlayList:</label>
-                    <select name="playlist" id="select-playlist">
-                        <option>-- Chọn PlayList --</option>
-                        <option>Nhạc nhẹ</option>
-                        <option>Nhạc Chill</option>
-                        <option>Nhạc Bolero</option>
-                    </select>
+                    <p>PlayList bị xóa sẽ không thể khôi phục. Bạn vẫn chắc chắn tiếp tục ?</p>
                 </div>
                 <div class="footer">
-                    <button class="btn btn-info">Xác nhận</button>
+                    <button class="btn btn-danger">Xóa</button>
                     <button class="btn btn-seco cancel">Hủy bỏ</button>
                 </div>
             </div>
